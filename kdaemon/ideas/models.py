06f8204dbe75b288143da3fb73fc336e93cdbf20
Giovanni Giorgi (macmini) <jj@gioorgi.com>
2022-06-04 10:28:52 +0200

    Date created on idea object, now is auto-populated. Added reference to github pages

--- kdaemon/ideas/models.py
+++ kdaemon/ideas/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+import django.utils
 # Create your models here.
 class Idea(models.Model):
     title=models.CharField(max_length=160)
     description=models.TextField()
-    creation_date=models.DateTimeField('date created')
+    creation_date=models.DateTimeField('date created', default=django.utils.timezone.now)
     def __str__(self):
         return self.title
